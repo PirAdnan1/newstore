0c6d807a76356551b85e2a090195c6889d6456e4
PirAdnan1 <piradnan113@gmail.com>
2025-11-04 07:10:05 -0800

    fix hero

--- app/components/Home/Hero.jsx
+++ app/components/Home/Hero.jsx
@@ -6,10 +6,10 @@ import HeroImage from "@/app/assets/hero.png"
 function Hero() {
     return (
         <div>
-            <div className='bg-black flex items-center text-white'>
-                <Image src={HeroImage} />
-                <div>
-                    <h1 className='md:text-3xl lg:text-5xl'>New Collections of Fashion Clothes</h1>
+            <div className='bg-black flex flex-col md:flex-row items-center text-white pt-4 md:pt-0'>
+                <Image src={HeroImage} className='order-2 md:order-1' />
+                <div className='order-1 md:order-2'>
+                    <h1 className='md:text-3xl lg:text-5xl text-xl font-semibold'>New Collections of Fashion Clothes</h1>
                     <p className='text-base'>Find fashion-forward outfits perfect for every occasion</p>
                 </div>
             </div>
